b1486e72bf144317545f53bbe1658fd474ede001
mathieu arnaud <mathieuarnaud@rocketmail.com>
2024-08-27 11:56:42 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias quasi
+        vitae dolorum magnam nesciunt laborum at. Obcaecati nesciunt eum odit
+        culpa similique tempora blanditiis earum, architecto velit optio
+        repellendus delectus animi saepe expedita tempore modi numquam
+        laboriosam pariatur! Autem labore quae amet pariatur laudantium iure
+        sequi voluptate voluptas corrupti incidunt! Vero asperiores, sed
+        aspernatur facere numquam repudiandae a suscipit non. Animi veniam
+        voluptate harum porro, ex officiis, nobis commodi ab adipisci officia
+        consequatur magni quod rerum mollitia error eius distinctio odio? Quae
+        quaerat libero doloremque, deleniti voluptatum dolore inventore
+        consectetur nostrum ut excepturi minus facilis atque rem quos,
+        aspernatur veniam?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit
+        ratione saepe accusamus earum facere. Accusantium ipsa, iste rem
+        doloribus explicabo magni maxime ipsam. Provident dolores aliquam
+        repellat, in reiciendis similique error possimus ducimus totam vero vel
+        accusantium ipsam unde tempore expedita voluptatibus sint neque nisi
+        amet cupiditate facilis obcaecati. Laborum voluptatum laudantium,
+        quaerat incidunt sit ducimus. Temporibus facilis nulla numquam odio
+        assumenda. Rem nobis corrupti blanditiis magni architecto qui reiciendis
+        nisi ullam pariatur. Provident aliquam mollitia, aperiam consectetur
+        quisquam molestias quae iure quod minus debitis placeat illum iste ipsa
+        hic maxime, assumenda harum modi maiores explicabo soluta quia.
+        Excepturi enim temporibus saepe beatae. Assumenda optio nostrum
+        similique pariatur? Molestias, dolorem. Quis repellat fugit neque aut
+        illo qui est iste? Placeat aperiam unde ab repellendus explicabo omnis
+        exercitationem delectus blanditiis, esse culpa voluptas rem harum sint
+        illum et, dolorum consequuntur commodi quo molestiae reprehenderit qui
+        amet doloremque! Dolore illum incidunt aliquid. Suscipit natus assumenda
+        voluptas amet! Numquam ut consequatur quos nobis id consequuntur debitis
+        aspernatur dolores, perferendis cumque. Minima reiciendis voluptate
+        exercitationem enim eius maxime omnis, quibusdam nisi blanditiis
+        consequuntur? Optio provident excepturi porro, error, deserunt officiis
+        recusandae fugit tempore perspiciatis temporibus, esse corrupti magnam
+        dolor natus velit nihil atque sequi!
+      </p>
+    </div>
+  );
+};
+
+export default About;
